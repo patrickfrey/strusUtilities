854be42ff9db8f2b91b59f1a73dea8df1128f93d
patrickfrey <patrickpfrey@yahoo.com>
2015-05-07 10:18:32 +0200

    fix wrong options declaration in dump storage

--- src/strusDumpStorage/strusDumpStorage.cpp
+++ src/strusDumpStorage/strusDumpStorage.cpp
@@ -80,7 +80,7 @@ int main( int argc, const char* argv[])
 		opt = strus::ProgramOptions(
 				argc, argv, 6,
 				"h,help", "v,version", "m,module:", "M,moduledir:",
-				"r,rpc:", "s,storage");
+				"r,rpc:", "s,storage:");
 		if (opt( "help")) printUsageAndExit = true;
 		if (opt( "version"))
 		{
