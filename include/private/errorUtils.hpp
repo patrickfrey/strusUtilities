fe56073683d0d85b7054bffa28e5177fe1fd3c76
patrickfrey <patrickpfrey@yahoo.com>
2018-03-12 22:20:24 +0100

    changed definition of error codes in strusbase (no structure anymore)

--- include/private/errorUtils.hpp
+++ include/private/errorUtils.hpp
@@ -13,7 +13,6 @@
 #include "private/internationalization.hpp"
 
 #define THIS_COMPONENT_NAME	"strus utilities"
-#define THIS_COMPONENT_ID	strus::StrusComponentUtilities
 
 /// \brief strus toplevel namespace
 namespace strus
@@ -22,80 +21,80 @@ namespace strus
 #define CATCH_ERROR_MAP( contextExplainText, errorBuffer)\
 	catch (const std::bad_alloc&)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseOutOfMem), _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		(errorBuffer).report( strus::ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
 	}\
 	catch (const std::runtime_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseRuntimeError), contextExplainText, err.what());\
+		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, err.what());\
 	}\
 	catch (const std::logic_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseLogicError), _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 	}\
 	catch (const std::exception& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseUncaughtException), _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 	}
 
 #define CATCH_ERROR_ARG1_MAP( contextExplainText, ARG, errorBuffer)\
 	catch (const std::bad_alloc&)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseOutOfMem), _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		(errorBuffer).report( strus::ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
 	}\
 	catch (const std::runtime_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseRuntimeError), contextExplainText, ARG, err.what());\
+		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, ARG, err.what());\
 	}\
 	catch (const std::logic_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseLogicError), _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 	}\
 	catch (const std::exception& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseUncaughtException), _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 	}
 
 #define CATCH_ERROR_MAP_RETURN( contextExplainText, errorBuffer, errorReturnValue)\
 	catch (const std::bad_alloc&)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseOutOfMem), _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		(errorBuffer).report( strus::ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
 		return errorReturnValue;\
 	}\
 	catch (const std::runtime_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseRuntimeError), contextExplainText, err.what());\
+		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, err.what());\
 		return errorReturnValue;\
 	}\
 	catch (const std::logic_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseLogicError), _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 		return errorReturnValue;\
 	}\
 	catch (const std::exception& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseUncaughtException), _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 		return errorReturnValue;\
 	}
 
 #define CATCH_ERROR_ARG1_MAP_RETURN( contextExplainText, ARG, errorBuffer, errorReturnValue)\
 	catch (const std::bad_alloc&)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseOutOfMem), _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		(errorBuffer).report( strus::ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
 		return errorReturnValue;\
 	}\
 	catch (const std::runtime_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseRuntimeError), contextExplainText, ARG, err.what());\
+		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, ARG, err.what());\
 		return errorReturnValue;\
 	}\
 	catch (const std::logic_error& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseLogicError), _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 		return errorReturnValue;\
 	}\
 	catch (const std::exception& err)\
 	{\
-		(errorBuffer).report( *strus::ErrorCode(THIS_COMPONENT_ID,strus::ErrorOperationUnknown,strus::ErrorCauseUncaughtException), _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 		return errorReturnValue;\
 	}
 
