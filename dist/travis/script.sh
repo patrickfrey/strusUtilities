b4e707437d12a1d18e48776e00bb6f1e676a7a87
PatrickFrey <patrickpfrey@yahoo.com>
2019-01-19 10:40:37 +0100

    fixed some issues with building hyperscan in travis build that appeared with latest changes

--- dist/travis/script.sh
+++ dist/travis/script.sh
@@ -6,6 +6,43 @@ OS=$(uname -s)
 
 PROJECT=strusUtilities
 
+build_dep_project() {
+	prj_cmakeflags=$1
+	case $OS in
+		Linux)
+			mkdir build
+			cd build
+			cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release \
+				-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g $prj_cmakeflags \
+				..
+			make
+			sudo make install
+			cd ..
+			;;
+	
+		Darwin)
+			# forcing brew versions (of gettext) over Mac versions
+			export CFLAGS=-I/usr/local
+			export CXXFLAGS=-I/usr/local
+			export LDFLAGS=-L/usr/local/lib
+			mkdir build
+			cd build
+			cmake \
+				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_CXX_FLAGS='-g -Wno-error=format-nonliteral -Wno-error=format-security' -G Xcode $prj_cmakeflags \
+				..
+			xcodebuild -configuration Release -target ALL_BUILD
+			xcodebuild -configuration Release -target RUN_TESTS
+			sudo xcodebuild -configuration Release -target install
+			cd ..
+			;;
+			
+		*)
+			echo "ERROR: unknown operating system '$OS'."
+			;;
+	esac
+}
+
 build_strus_project() {
 	prj_cmakeflags=$1
 	case $OS in
@@ -22,40 +59,20 @@ build_strus_project() {
 			;;
 	
 		Darwin)
-			if test "X$CC" = "Xgcc"; then
-				# gcc on OSX is a mere frontend to clang, force using gcc 4.8
-				export CXX=g++-4.8
-				export CC=gcc-4.8
-				# forcing brew versions (of gettext) over Mac versions
-				export CFLAGS=-I/usr/local
-				export CXXFLAGS=-I/usr/local
-				export LDFLAGS=-L/usr/local/lib
-				mkdir build
-				cd build
-				cmake \
-					-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
-					-DCMAKE_CXX_FLAGS=-g -G 'Unix Makefiles' $prj_cmakeflags \
-					..
-				make VERBOSE=1
-				make VERBOSE=1 CTEST_OUTPUT_ON_FAILURE=1 test
-				sudo make VERBOSE=1 install
-				cd ..
-			else
-				# forcing brew versions (of gettext) over Mac versions
-				export CFLAGS=-I/usr/local
-				export CXXFLAGS=-I/usr/local
-				export LDFLAGS=-L/usr/local/lib
-				mkdir build
-				cd build
-				cmake \
-					-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
-					-DCMAKE_CXX_FLAGS=-g -G Xcode $prj_cmakeflags \
-					..
-				xcodebuild -configuration Release -target ALL_BUILD
-				xcodebuild -configuration Release -target RUN_TESTS
-				sudo xcodebuild -configuration Release -target install
-				cd ..
-			fi
+			# forcing brew versions (of gettext) over Mac versions
+			export CFLAGS=-I/usr/local
+			export CXXFLAGS=-I/usr/local
+			export LDFLAGS=-L/usr/local/lib
+			mkdir build
+			cd build
+			cmake \
+				-DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_CXX_FLAGS=-g -G Xcode $prj_cmakeflags \
+				..
+			xcodebuild -configuration Release -target ALL_BUILD
+			xcodebuild -configuration Release -target RUN_TESTS
+			sudo xcodebuild -configuration Release -target install
+			cd ..
 			;;
 			
 		*)
@@ -92,6 +109,19 @@ setup_env
 
 # build pre-requisites
 DEPS="strusBase strus strusAnalyzer strusTrace strusModule strusRpc"
+
+if test "x$STRUS_WITH_PATTERN" = "xYES"; then
+	# install hyperscan
+	git clone https://github.com/intel/hyperscan.git
+	cd hyperscan
+	build_dep_project ""
+	cd ..
+
+	DEPS="${DEPS} strusPattern"
+fi
+if test "x$STRUS_WITH_VECTOR" = "xYES"; then
+	DEPS="${DEPS} strusVector"
+fi
 
 GITURL=`git config remote.origin.url`
 cd ..
