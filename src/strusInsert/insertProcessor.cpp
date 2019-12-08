32597c230859d4c393de5bad1a914c6045153b82
PatrickFrey <patrickpfrey@yahoo.com>
2019-12-08 12:09:56 +0100

    adapt changes in error buffer interface, do not cut documents too big in inserter. This is done by the core issuing an info message when it is done

--- src/strusInsert/insertProcessor.cpp
+++ src/strusInsert/insertProcessor.cpp
@@ -13,7 +13,6 @@
 #include "strus/documentAnalyzerContextInterface.hpp"
 #include "strus/textProcessorInterface.hpp"
 #include "strus/storageClientInterface.hpp"
-#include "strus/storageTransactionInterface.hpp"
 #include "strus/metaDataReaderInterface.hpp"
 #include "private/fileCrawlerInterface.hpp"
 #include "strus/errorBufferInterface.hpp"
@@ -39,7 +38,7 @@ InsertProcessor::InsertProcessor(
 		const analyzer::DocumentClass& defaultDocumentClass_,
 		CommitQueue* commitque_,
 		FileCrawlerInterface* crawler_,
-		unsigned int transactionSize_,
+		int transactionSize_,
 		bool verbose_,
 		ErrorBufferInterface* errorhnd_)
 	:m_storage(storage_)
@@ -48,7 +47,9 @@ InsertProcessor::InsertProcessor(
 	,m_defaultDocumentClass(defaultDocumentClass_)
 	,m_commitque(commitque_)
 	,m_crawler(crawler_)
+	,m_transaction()
 	,m_transactionSize(transactionSize_)
+	,m_docCount(0)
 	,m_verbose(verbose_)
 	,m_terminated(false)
 	,m_errorhnd(errorhnd_)
@@ -63,17 +64,171 @@ void InsertProcessor::sigStop()
 	m_terminated.set( true);
 }
 
+void InsertProcessor::processDocument( const std::string& filename)
+{
+	strus::InputStream input( filename);
+	strus::local_ptr<strus::DocumentAnalyzerContextInterface> analyzerContext;
+	strus::analyzer::DocumentClass dclass;
+	if (!m_defaultDocumentClass.defined())
+	{
+		// Read the input file to analyze and detect its document type:
+		char hdrbuf[ 4096];
+		std::size_t hdrsize = input.readAhead( hdrbuf, sizeof( hdrbuf));
+		if (input.error())
+		{
+			std::cerr << string_format( _TXT( "failed to read document file '%s': %s"), filename.c_str(), ::strerror( input.error())) << std::endl; 
+			return;
+		}
+		if (!m_textproc->detectDocumentClass( dclass, hdrbuf, hdrsize, hdrsize < sizeof(hdrbuf)))
+		{
+			std::cerr << string_format( _TXT( "failed to detect document class of file '%s'"), filename.c_str()) << std::endl; 
+			return;
+		}
+	}
+	else
+	{
+		dclass = m_defaultDocumentClass;
+	}
+	const strus::DocumentAnalyzerInstanceInterface* analyzer = m_analyzerMap->get( dclass);
+	if (!analyzer)
+	{
+		std::cerr << string_format( _TXT( "no analyzer defined for document class with MIME type '%s' schema '%s'"), dclass.mimeType().c_str(), dclass.schema().c_str()) << std::endl; 
+		return;
+	}
+	analyzerContext.reset( analyzer->createContext( dclass));
+	if (!analyzerContext.get()) throw strus::runtime_error( _TXT("error creating analyzer context: %s"), m_errorhnd->fetchError());
+
+	// Analyze the document (with subdocuments) and insert it:
+	enum {AnalyzerBufSize=8192};
+	char buf[ AnalyzerBufSize];
+	bool eof = false;
+
+	while (!eof)
+	{
+		std::size_t readsize = input.read( buf, sizeof(buf));
+		if (readsize < sizeof(buf))
+		{
+			if (input.error())
+			{
+				std::cerr << string_format( _TXT( "failed to read document file '%s': %s"), filename.c_str(), ::strerror( input.error())) << std::endl; 
+				break;
+			}
+			eof = true;
+		}
+		analyzerContext->putInput( buf, readsize, eof);
+		// Analyze the document and print the result:
+		strus::analyzer::Document doc;
+		while (!m_terminated.test() && analyzerContext->analyzeNext( doc))
+		{
+			// Create the document with the correct docid:
+			std::vector<strus::analyzer::DocumentAttribute>::const_iterator
+				oi = doc.attributes().begin(),
+				oe = doc.attributes().end();
+			for (;oi != oe
+				&& oi->name() != strus::Constants::attribute_docid();
+				++oi){}
+			const char* docid = 0;
+			strus::local_ptr<strus::StorageDocumentInterface> storagedoc;
+			if (oi != oe)
+			{
+				storagedoc.reset( m_transaction->createDocument( oi->value()));
+				if (!storagedoc.get()) throw strus::runtime_error( _TXT("error creating document: %s"), m_errorhnd->fetchError());
+				docid = oi->value().c_str();
+				//... use the docid from the analyzer if defined there
+			}
+			else
+			{
+				storagedoc.reset( m_transaction->createDocument( filename));
+				if (!storagedoc.get()) throw strus::runtime_error( _TXT("error creating document"));
+				storagedoc->setAttribute( strus::Constants::attribute_docid(), filename);
+				docid = filename.c_str();
+				//... define file path as hardcoded docid attribute
+			}
+			// Define all search index term occurrencies:
+			std::vector<strus::analyzer::DocumentTerm>::const_iterator
+				ti = doc.searchIndexTerms().begin(),
+				te = doc.searchIndexTerms().end();
+			for (; ti != te; ++ti)
+			{
+				storagedoc->addSearchIndexTerm( ti->type(), ti->value(), ti->pos());
+			}
+			// Define all search index structures:
+			std::vector<strus::analyzer::DocumentStructure>
+				structlist = doc.searchIndexStructures();
+			std::vector<strus::analyzer::DocumentStructure>::const_iterator
+				si = structlist.begin(), se = structlist.end();
+			for (; si != se; ++si)
+			{
+				strus::IndexRange source( si->source().start(),si->source().end());
+				strus::IndexRange sink( si->sink().start(),si->sink().end());
+				storagedoc->addSearchIndexStructure( si->name(), source, sink);
+			}
+			// Define all forward index terms:
+			std::vector<strus::analyzer::DocumentTerm>::const_iterator
+				fi = doc.forwardIndexTerms().begin(),
+				fe = doc.forwardIndexTerms().end();
+			for (; fi != fe; ++fi)
+			{
+				storagedoc->addForwardIndexTerm( fi->type(), fi->value(), fi->pos());
+			}
+			// Define all attributes extracted from the document analysis:
+			std::vector<strus::analyzer::DocumentAttribute>::const_iterator
+				ai = doc.attributes().begin(), ae = doc.attributes().end();
+			for (; ai != ae; ++ai)
+			{
+				storagedoc->setAttribute( ai->name(), ai->value());
+			}
+			// Define all metadata elements extracted from the document analysis:
+			std::vector<strus::analyzer::DocumentMetaData>::const_iterator
+				mi = doc.metadata().begin(), me = doc.metadata().end();
+			for (; mi != me; ++mi)
+			{
+				NumericVariant val = mi->value();
+				storagedoc->setMetaData( mi->name(), val);
+			}
+			// Finish document completed:
+			storagedoc->done();
+			if (m_errorhnd->hasInfo())
+			{
+				std::vector<std::string> info = m_errorhnd->fetchInfo();
+				std::vector<std::string>::const_iterator ei = info.begin(), ee = info.end();
+				for (; ei != ee; ++ei)
+				{
+					std::cerr << strus::string_format( _TXT( "%s in document '%s'"), ei->c_str(), docid) << std::endl;
+				}
+			}
+			if (m_errorhnd->hasError())
+			{
+				const char* errmsg = m_errorhnd->fetchError();
+				throw strus::runtime_error( _TXT( "failed to process document '%s' in file %s': %s"), docid, filename.c_str(), errmsg);
+			}
+			m_docCount++;
+			if (m_docCount == m_transactionSize && m_docCount && !m_terminated.test())
+			{
+				m_commitque->pushTransaction( m_transaction.get());
+				m_transaction.release();
+				m_transaction.reset( m_storage->createTransaction());
+				if (!m_transaction.get()) throw strus::runtime_error( _TXT("error recreating storage transaction: %s"), m_errorhnd->fetchError());
+				m_docCount = 0;
+			}
+		}
+	}
+	if (m_verbose)
+	{
+		std::cerr << "processed file '" << filename << "' (" << m_docCount << ")" << std::endl;
+	}
+}
+
 void InsertProcessor::run()
 {
-	unsigned int docCount = 0;
+	m_docCount = 0;
 	try
 	{
 		std::vector<std::string> files;
 		std::vector<std::string>::const_iterator fitr;
 	
-		strus::local_ptr<strus::StorageTransactionInterface>
-			transaction( m_storage->createTransaction());
-		if (!transaction.get()) throw std::runtime_error( _TXT("error creating storage transaction"));
+		m_transaction.reset( m_storage->createTransaction());
+		if (!m_transaction.get()) throw strus::runtime_error( _TXT("error creating storage transaction: %s"), m_errorhnd->fetchError());
 
 		while (!(files=m_crawler->fetch()).empty())
 		{
@@ -82,197 +237,14 @@ void InsertProcessor::run()
 			{
 				try
 				{
-					strus::InputStream input( *fitr);
-					strus::local_ptr<strus::DocumentAnalyzerContextInterface> analyzerContext;
-					strus::analyzer::DocumentClass dclass;
-					if (!m_defaultDocumentClass.defined())
-					{
-						// Read the input file to analyze and detect its document type:
-						char hdrbuf[ 4096];
-						std::size_t hdrsize = input.readAhead( hdrbuf, sizeof( hdrbuf));
-						if (input.error())
-						{
-							std::cerr << string_format( _TXT( "failed to read document file '%s': %s"), fitr->c_str(), ::strerror( input.error())) << std::endl; 
-							continue;
-						}
-						if (!m_textproc->detectDocumentClass( dclass, hdrbuf, hdrsize, hdrsize < sizeof(hdrbuf)))
-						{
-							std::cerr << string_format( _TXT( "failed to detect document class of file '%s'"), fitr->c_str()) << std::endl; 
-							continue;
-						}
-					}
-					else
-					{
-						dclass = m_defaultDocumentClass;
-					}
-					const strus::DocumentAnalyzerInstanceInterface* analyzer = m_analyzerMap->get( dclass);
-					if (!analyzer)
-					{
-						std::cerr << string_format( _TXT( "no analyzer defined for document class with MIME type '%s' schema '%s'"), dclass.mimeType().c_str(), dclass.schema().c_str()) << std::endl; 
-						continue;
-					}
-					analyzerContext.reset( analyzer->createContext( dclass));
-					if (!analyzerContext.get()) throw std::runtime_error( _TXT("error creating analyzer context"));
-
-					// Analyze the document (with subdocuments) and insert it:
-					enum {AnalyzerBufSize=8192};
-					char buf[ AnalyzerBufSize];
-					bool eof = false;
-	
-					while (!eof)
-					{
-						std::size_t readsize = input.read( buf, sizeof(buf));
-						if (readsize < sizeof(buf))
-						{
-							if (input.error())
-							{
-								std::cerr << string_format( _TXT( "failed to read document file '%s': %s"), fitr->c_str(), ::strerror( input.error())) << std::endl; 
-								break;
-							}
-							eof = true;
-						}
-						analyzerContext->putInput( buf, readsize, eof);
-						// Analyze the document and print the result:
-						strus::analyzer::Document doc;
-						while (!m_terminated.test() && analyzerContext->analyzeNext( doc))
-						{
-							// Create the storage transaction document with the correct docid:
-							std::vector<strus::analyzer::DocumentAttribute>::const_iterator
-								oi = doc.attributes().begin(),
-								oe = doc.attributes().end();
-							for (;oi != oe
-								&& oi->name() != strus::Constants::attribute_docid();
-								++oi){}
-							const char* docid = 0;
-							strus::local_ptr<strus::StorageDocumentInterface> storagedoc;
-							if (oi != oe)
-							{
-								storagedoc.reset( transaction->createDocument( oi->value()));
-								if (!storagedoc.get()) throw std::runtime_error( _TXT("error creating document"));
-								docid = oi->value().c_str();
-								//... use the docid from the analyzer if defined there
-							}
-							else
-							{
-								storagedoc.reset( transaction->createDocument( *fitr));
-								if (!storagedoc.get()) throw std::runtime_error( _TXT("error creating document"));
-								storagedoc->setAttribute(
-									strus::Constants::attribute_docid(), *fitr);
-								docid = fitr->c_str();
-								//... define file path as hardcoded docid attribute
-							}
-							int maxpos = 0;
-							// Define all search index term occurrencies:
-							std::vector<strus::analyzer::DocumentTerm>::const_iterator
-								ti = doc.searchIndexTerms().begin(),
-								te = doc.searchIndexTerms().end();
-							for (; ti != te; ++ti)
-							{
-								if (ti->pos() > (int)Constants::storage_max_position_info())
-								{
-									// Cut positions away that are out of range.
-									//	Issue a warning later:
-									if (ti->pos() > maxpos)
-									{
-										maxpos = ti->pos();
-									}
-								}
-								else
-								{
-									storagedoc->addSearchIndexTerm(
-										ti->type(), ti->value(), ti->pos());
-								}
-							}
-							// Define all search index structures:
-							std::vector<strus::analyzer::DocumentStructure>
-								structlist = doc.searchIndexStructures();
-							std::vector<strus::analyzer::DocumentStructure>::const_iterator
-								si = structlist.begin(), se = structlist.end();
-							for (; si != se; ++si)
-							{
-								if (si->source().end() > (int)Constants::storage_max_position_info()
-								||  si->sink().end() > (int)Constants::storage_max_position_info())
-								{}
-								else
-								{
-									strus::IndexRange source( si->source().start(),si->source().end());
-									strus::IndexRange sink( si->sink().start(),si->sink().end());
-									storagedoc->addSearchIndexStructure( si->name(), source, sink);
-								}
-							}
-							// Define all forward index terms:
-							std::vector<strus::analyzer::DocumentTerm>::const_iterator
-								fi = doc.forwardIndexTerms().begin(),
-								fe = doc.forwardIndexTerms().end();
-							for (; fi != fe; ++fi)
-							{
-								if (fi->pos() > (int)Constants::storage_max_position_info())
-								{
-									// Cut positions away that are out of range. Issue a warning later:
-									if (fi->pos() > maxpos)
-									{
-										maxpos = fi->pos();
-									}
-								}
-								else
-								{
-									storagedoc->addForwardIndexTerm(
-										fi->type(), fi->value(), fi->pos());
-								}
-							}
-		
-							// Define all attributes extracted from the document analysis:
-							std::vector<strus::analyzer::DocumentAttribute>::const_iterator
-								ai = doc.attributes().begin(), ae = doc.attributes().end();
-							for (; ai != ae; ++ai)
-							{
-								storagedoc->setAttribute( ai->name(), ai->value());
-							}
-	
-							// Define all metadata elements extracted from the document analysis:
-							std::vector<strus::analyzer::DocumentMetaData>::const_iterator
-								mi = doc.metadata().begin(), me = doc.metadata().end();
-							for (; mi != me; ++mi)
-							{
-								NumericVariant val = mi->value();
-								storagedoc->setMetaData( mi->name(), val);
-							}
-							// Issue warning for documents cut because they are too big to insert:
-							if (maxpos > (int)Constants::storage_max_position_info())
-							{
-								std::cerr << string_format( _TXT( "token positions of document '%s' are out or range (document too big, %u token positions assigned)"), docid, maxpos) << std::endl;
-							}
-	
-							// Finish document completed:
-							storagedoc->done();
-							docCount++;
-
-							if (docCount == m_transactionSize && docCount && !m_terminated.test())
-							{
-								m_commitque->pushTransaction( transaction.get());
-								transaction.release();
-								transaction.reset( m_storage->createTransaction());
-								if (!transaction.get()) throw std::runtime_error( _TXT("error recreating storage transaction"));
-								docCount = 0;
-							}
-						}
-						if (m_errorhnd->hasError())
-						{
-							const char* errmsg = m_errorhnd->fetchError();
-							throw strus::runtime_error( _TXT( "failed to process document '%s': %s"), fitr->c_str(), errmsg);
-						}
-					}
-					if (m_verbose)
-					{
-						std::cerr << "processed file '" << *fitr << "' (" << docCount << ")" << std::endl;
-					}
+					processDocument( *fitr);
 				}
 				catch (const std::bad_alloc& err)
 				{
 					std::cerr << string_format( _TXT( "failed to process document '%s': memory allocation error"), fitr->c_str()) << std::endl;
-					transaction.reset( m_storage->createTransaction());
-					if (!transaction.get()) throw std::runtime_error( _TXT("error recreating storage transaction"));
-					docCount = 0;
+					m_transaction.reset( m_storage->createTransaction());
+					if (!m_transaction.get()) throw strus::runtime_error( _TXT("error recreating storage transaction: %s"), m_errorhnd->fetchError());
+					m_docCount = 0;
 				}
 				catch (const std::runtime_error& err)
 				{
@@ -285,17 +257,17 @@ void InsertProcessor::run()
 					{
 						std::cerr << string_format( _TXT( "failed to process document '%s': %s"), fitr->c_str(), err.what()) << std::endl;
 					}
-					transaction.reset( m_storage->createTransaction());
-					if (!transaction.get()) throw std::runtime_error( _TXT("error recreating storage transaction"));
-					docCount = 0;
+					m_transaction.reset( m_storage->createTransaction());
+					if (!m_transaction.get()) throw strus::runtime_error( _TXT("error recreating storage transaction: %s"), m_errorhnd->fetchError());
+					m_docCount = 0;
 				}
 			}
 		}
-		if (!m_terminated.test() && docCount)
+		if (!m_terminated.test() && m_docCount)
 		{
-			m_commitque->pushTransaction( transaction.get());
-			transaction.release();
-			docCount = 0;
+			m_commitque->pushTransaction( m_transaction.get());
+			m_transaction.release();
+			m_docCount = 0;
 		}
 	}
 	catch (const std::bad_alloc& err)
--- src/strusInsert/insertProcessor.hpp
+++ src/strusInsert/insertProcessor.hpp
@@ -8,6 +8,8 @@
 #ifndef _STRUS_INSERTER_PROCESSOR_HPP_INCLUDED
 #define _STRUS_INSERTER_PROCESSOR_HPP_INCLUDED
 #include "strus/base/atomic.hpp"
+#include "strus/reference.hpp"
+#include "strus/storageTransactionInterface.hpp"
 #include "private/documentAnalyzer.hpp"
 
 namespace strus {
@@ -36,7 +38,7 @@ public:
 			const analyzer::DocumentClass& defaultDocumentClass_,
 			CommitQueue* commitque_,
 			FileCrawlerInterface* crawler_,
-			unsigned int transactionSize_,
+			int transactionSize_,
 			bool verbose_,
 			ErrorBufferInterface* errorhnd_);
 
@@ -46,13 +48,18 @@ public:
 	void run();
 
 private:
+	void processDocument( const std::string& filename);
+
+private:
 	StorageClientInterface* m_storage;
 	const TextProcessorInterface* m_textproc;
 	const strus::DocumentAnalyzer* m_analyzerMap;
 	analyzer::DocumentClass m_defaultDocumentClass;
 	CommitQueue* m_commitque;
 	FileCrawlerInterface* m_crawler;
-	unsigned int m_transactionSize;
+	strus::Reference<strus::StorageTransactionInterface> m_transaction;
+	int m_transactionSize;
+	int m_docCount;
 	bool m_verbose;
 	strus::AtomicFlag m_terminated;
 	ErrorBufferInterface* m_errorhnd;
