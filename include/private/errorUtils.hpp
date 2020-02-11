1b349e37efd0efe60669096f799f328402ebbafd
PatrickFrey <patrickpfrey@yahoo.com>
2020-02-11 19:29:10 +0100

    added some error utility macros

--- include/private/errorUtils.hpp
+++ include/private/errorUtils.hpp
@@ -11,6 +11,7 @@
 #define _STRUS_UTILITIES_ERROR_UTILITIES_HPP_INCLUDED
 #include <stdexcept>
 #include "private/internationalization.hpp"
+#include "strus/errorBufferInterface.hpp"
 
 #define THIS_COMPONENT_NAME	"strus utilities"
 
@@ -54,6 +55,24 @@ namespace strus
 		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
 	}
 
+#define CATCH_ERROR_ARG2_MAP( contextExplainText, ARG1, ARG2, errorBuffer)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, ARG1, ARG2, err.what());\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+	}
+
 #define CATCH_ERROR_MAP_RETURN( contextExplainText, errorBuffer, errorReturnValue)\
 	catch (const std::bad_alloc&)\
 	{\
@@ -85,6 +104,28 @@ namespace strus
 	catch (const std::runtime_error& err)\
 	{\
 		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, ARG, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		return errorReturnValue;\
+	}
+
+#define CATCH_ERROR_ARG2_MAP_RETURN( contextExplainText, ARG1, ARG2, errorBuffer, errorReturnValue)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		return errorReturnValue;\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( strus::ErrorCodeRuntimeError, contextExplainText, ARG1, ARG2, err.what());\
 		return errorReturnValue;\
 	}\
 	catch (const std::logic_error& err)\
